1756521431b526b857244173d98582ea142f64d1
Yenlik <army.ibragim@gmail.com>
2022-07-04 10:36:48 +0600

    commit

--- src/components/opportunities.js
+++ src/components/opportunities.js
@@ -3,34 +3,12 @@ import BoxImg from "../images/Group 746.png";
 import BoxImg2 from "../images/Group 747.png";
 import BoxImg3 from "../images/Group 748.png";
 import Arrow from "../images/Arrow 8.png";
-import styles from "./styles/opportunities.module.css";
+import styles from "./opportunities.module.css";
 const Opportunities = () => {
   const classes = styles.point + " " + styles.active;
 
   const [offset, setOffset] = useState(0);
 
-  const [touchStart, setTouchStart] = useState(null);
-  const [touchEnd, setTouchEnd] = useState(null);
-
-  // the required distance between touchStart and touchEnd to be detected as a swipe
-  const minSwipeDistance = 50;
-
-  const onTouchStart = (e) => {
-    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
-    setTouchStart(e.targetTouches[0].clientX);
-  };
-
-  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX);
-
-  const onTouchEnd = () => {
-    if (!touchStart || !touchEnd) return;
-    const distance = touchStart - touchEnd;
-    const isLeftSwipe = distance > minSwipeDistance;
-    const isRightSwipe = distance < -minSwipeDistance;
-    if (isLeftSwipe || isRightSwipe) isLeftSwipe ? RightClick() : LeftClick();
-
-    // add your conditional logic here
-  };
   const RightClick = () => {
     console.log("Right clicked");
     setOffset((current) => {
